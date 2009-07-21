bc246f04be1f12db53043774fd62082c690a39dd
Mike <parabuzzle@gmail.com>
2009-07-20 19:10:24 -0700

    added license

--- src/collectors/udpcollector.rb
+++ src/collectors/udpcollector.rb
@@ -1,11 +1,13 @@
 # Cistern UDP Collector application
-# This application is part of the Cistern Application
+# This file is part of the Cistern Application
 #
 # Author - Mike Heijmans (parabuzzle@yahoo.com)
 #
+# Licensed under the MIT License
+# Copyright (c) 2009 Michael Heijmans
 
 require 'socket'
-port = 12000
+port = 11111
 server = UDPSocket.open
 server.bind(nil, port)
 loop do
