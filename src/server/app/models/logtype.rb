ac18880974d0aec15613807fd9b24a2faf17188b
Mike <parabuzzle@gmail.com>
2009-07-29 23:09:00 -0700

    Proof of concept on the log collector and the database schema. Need to make better delimiters and better concurrent handling

--- src/server/app/models/logtype.rb
+++ src/server/app/models/logtype.rb
@@ -1,4 +1,4 @@
 class Logtype < ActiveRecord::Base
   has_many :staticentries
-  
+  has_many :events, :through => :staticentries
 end
--- src/server/config/initializers/collector.rb
+++ src/server/config/initializers/collector.rb
@@ -1,14 +1,30 @@
 require 'digest/md5'
 module CollectionServer
-  
+
    def post_init
     ActiveRecord::Base.logger.info "-- someone connected to the collector"
    end
 
    def receive_data(data)
+     n = 1
         (@buffer ||= BufferedTokenizer.new(delimiter = "__1_EE")).extract(data).each do |line|
-          #receive_line(line)
-          puts line
+          start = Time.now.to_i
+          raw = line.split('__1_B')
+          map = Hash.new
+          raw.each do |keys|
+            parts = keys.split('=')
+            map.store(parts[0],parts[1])
+          end
+          static = Logtype.find_by_name(map['logtype']).staticentries.new
+          static.data = map['data']
+          static.save
+          static = Staticentry.find_by_id(Digest::MD5.hexdigest(map['data']))
+          event = static.events.new
+          event.time = map['time']
+          event.loglevel = map['level']
+          event.payload = map['payload']
+          event.save
+          puts "done #{Time.now.to_i - start} #{n + 1}"
         end
       end
    
