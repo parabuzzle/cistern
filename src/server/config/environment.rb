e3b2ae5999b23f4e44f2e2b2a0f7a0bf0e584166
Mike <parabuzzle@gmail.com>
2009-08-05 17:46:39 -0700

    Pulled in the acts_as_ferret plugin instead of relying on the gem

--- src/server/config/environment.rb
+++ src/server/config/environment.rb
@@ -21,7 +21,7 @@ Rails::Initializer.run do |config|
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem "eventmachine"
   config.gem "will_paginate"
-  config.gem "acts_as_ferret"
+  #config.gem "acts_as_ferret"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
